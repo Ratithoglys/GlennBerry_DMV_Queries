520c2db682497fd85dbad3cac20551c754405244
Lénaïc JAOUEN <lenaic.jaouen@cheops.fr>
2019-12-05 17:40:12 +0100

    Update 2019-12-05T17:40:12.8461007+01:00

--- 2016.sql
+++ 2016.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2016 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: November 18, 2019
+-- Last Modified: December 4, 2019
 -- https://www.sqlskills.com/blogs/glenn/
 -- http://sqlserverperformance.wordpress.com/
 -- Twitter: GlennAlanBerry
