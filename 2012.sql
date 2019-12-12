1c0aede7486e99009c34970c062d4f96a1b065ba
Lénaïc JAOUEN <lenaic.jaouen@cheops.fr>
2019-12-12 09:59:04 +0100

    Update 2019-12-12T09:59:04.8489887+01:00

--- 2012.sql
+++ 2012.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2012 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: December 4, 2019
+-- Last Modified: December 11, 2019
 -- https://www.sqlskills.com/blogs/glenn/
 -- http://sqlserverperformance.wordpress.com/
 -- Twitter: GlennAlanBerry
