e8d1fa4185197fb93be65369900de4dbff83fb05
Lénaïc JAOUEN <lenaic.jaouen@cheops.fr>
2020-09-09 11:45:44 +0200

    Update 2020-09-09T11:45:44.2487688+02:00

--- 2019.sql
+++ 2019.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2019 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: September 2, 2020
+-- Last Modified: September 8, 2020
 -- https://glennsqlperformance.com/ 
 -- https://sqlserverperformance.wordpress.com/
 -- YouTube: https://bit.ly/2PkoAM1 
