c5077e68bc9b2a11222ff0ce90806cbb001dfc06
Lénaïc J <github.com@ebumna.net>
2023-11-03 06:00:21 +0100

    Update 2023-11-03T06:00:20.0503015+01:00

--- 2016SP2.sql
+++ 2016SP2.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2016 SP2 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: October 12, 2023
+-- Last Modified: November 2, 2023
 -- https://glennsqlperformance.com/
 -- https://sqlserverperformance.wordpress.com/
 -- YouTube: https://bit.ly/2PkoAM1 
